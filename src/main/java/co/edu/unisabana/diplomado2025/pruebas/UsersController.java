465d0ca770c322661348e00e469b9afdce9a5905
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2025-09-02 18:50:24 -0500

    Add variable

--- src/main/java/co/edu/unisabana/diplomado2025/pruebas/UsersController.java
+++ src/main/java/co/edu/unisabana/diplomado2025/pruebas/UsersController.java
@@ -1,15 +1,22 @@
 package co.edu.unisabana.diplomado2025.pruebas;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
+@Slf4j
 public class UsersController {
 
     private ServiceX serviceX;
+
+    @Value("${pod}")
+    private String nombrePod;
 
     @GetMapping("/users/{id}")
     public UserDTO consultarUsuario(@PathVariable int id) {
@@ -18,6 +25,7 @@ public class UsersController {
 
     @GetMapping("/usuario")
     public String consultarUsuario() {
-        return "Daniel S.";
+        log.info("Consultando usuario");
+        return "Daniel S. desde el pod" + nombrePod;
     }
 }
